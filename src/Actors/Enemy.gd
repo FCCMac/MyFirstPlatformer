f63952c9e1c3ad7afcf4528376361fae9dbefbe3
FCCMac <cmcaulay07@gmail.com>
2020-02-21 19:16:49 -0700

    Tutorial 1 Complete

---
+++ src/Actors/Enemy.gd
@@ -0,0 +1,20 @@
+extends Actor
+
+func _ready():
+	_velocity.x = -speed.x
+	set_physics_process(false)
+	
+func _on_StompDetector_body_entered(body):
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+		
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+
+func _physics_process(delta):
+	if is_on_wall():
+		_velocity.x *= -1.0
+		
+	_velocity.y += gravity * delta
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,5 +1,15 @@
 extends Actor
 
+export var stomp_impulse = 1000.0
+
+func _on_EnemyDetector_area_entered(area):
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+
+
+func _on_EnemyDetector_body_entered(body):
+	queue_free()
+
+
 func _physics_process(delta):
 	var is_jump_interrupted = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction = get_direction()
@@ -7,12 +17,14 @@ func _physics_process(delta):
 	
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 
+
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 	)
-	
+
+
 func calculate_move_velocity(
 		linear_velocity: Vector2,
 		speed: Vector2,
@@ -27,5 +39,15 @@ func calculate_move_velocity(
 		out.y = speed.y * direction.y
 	if is_jump_interrupted:
 		out.y = 0.0
+	return out
+
+
+func calculate_stomp_velocity(
+		linear_velocity: Vector2,
+		stomp_impulse: float
+	) -> Vector2:
+	
+	var out = linear_velocity
+	out.y = -stomp_impulse
 	return out
 
